180a5f1a5320f0274e0f0a3f058d7f2a70cf72b3
Brandon Salter <bsalter5444@gmail.com>
2025-08-20 23:27:44 -0400

    massive overhaul to the website

---
+++ scripts/togglepics2.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    const images1 = document.querySelectorAll('.poke-grid .image img');
+
+    const originalSources = Array.from(images1).map(img => img.src);
+
+    const images2 = [
+        "./images/nude/hallie3.png"
+    ];
+
+    let isSet1 = true;
+
+    toggleButton.addEventListener("click", function() {
+        if (isSet1) {
+            images1.forEach((img, index) => {
+                img.src = images2[index];
+            });
+            isSet1 = false;
+        } else {
+            images1.forEach((img, index) => {
+                img.src = originalSources[index];
+            });
+            isSet1 = true;
+        }
+    });
+});
